2957743e6202d2ec816fd9314f4ff02c09b84996
Oliveseater <91335900+Oliveseater@users.noreply.github.com>
2021-11-12 16:38:34 +0500

    Add files via upload

---
+++ WTF/WTF/People.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WTF
+{
+    public class People
+    {
+        public string lastname;
+        public string name;
+        public string patronymic;
+        public int age;
+        public int weight; //пусть будут целыми
+    }
+}
---
+++ WTF/WTF/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WTF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //кол-во сотрудников чей возраст кратен 3
+
+            List<int> age = new List<int>();
+
+            StreamReader sr = File.OpenText("humans.txt");
+            string rl = "";
+
+            while((rl = sr.ReadLine()) != null)
+            {
+                string[] words = rl.Split(new char[] { ' ' });
+                age.Add(int.Parse(words[3]));
+            }
+            try
+            {
+                var findold = age.Where(a => a % 3 == 0);
+                foreach (int a in findold)
+                    Console.WriteLine(findold);
+            }
+            catch
+            {
+                Console.WriteLine("Некоректные данные");
+            }
+            sr.Close();
+
+            /*
+            Console.WriteLine("Кол-во чисел в массиве: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double[] numbs = new double[a];
+            Console.WriteLine("Введите числа: ");
+            for (int i = 0; i < a; i++)
+            {
+                numbs[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("Разность чисел массива: " + numbs.Aggregate((x,y) => x - y));
+            */
+        }
+    }
+}
